bc3188971d508339e453bc3b498b47203b55b0da
cmfrankel <cfrankel@elon.edu>
2017-07-10 16:21:30 -0400

    Update 1LaunchpadWebScraper.py

--- 1LaunchpadWebScraper.py
+++ 1LaunchpadWebScraper.py
@@ -25,17 +25,19 @@
 #
 ################################################################
 # usage:
-# python 1LaunchpadWebScraper.py <datasource_id> <password>
+# python 1FSFWebScraper.py <datasource_id> <password>
 #
 # purpose:
-# grab a list of the projects on Launchpad
-# get the name, display name, and website of each project 
+# get the project number, name, url, real url, and short description of each FSF project
+# get the license information of each FSF project
+# get the index html of each FSF project
 ################################################################
 
 import re
 import sys
 import pymysql
 from bs4 import BeautifulSoup
+import ssl
 try:
     import urllib.request as urllib2
 except ImportError:
@@ -44,18 +46,47 @@ except ImportError:
 datasource_id = '272'
 
 
-def insert():
+def runProjects():
     try:
-        cursor.execute(insertQuery,
+        cursor.execute(insertProjectsQuery,
                        (datasource_id,
-                        name,
-                        displayName,
-                        web_link))
+                        proj_num,
+                        proj_unixname,
+                        url2,
+                        real_url,
+                        desc_short))
         db.commit()
-        print(name, " inserted into projects table!\n")
+        print(proj_unixname, " inserted into projects table!\n")
     except pymysql.Error as err:
         print(err)
         db.rollback()
+
+
+def runIndexes():
+    try:
+        cursor.execute(insertIndexesQuery,
+                       (proj_num,
+                        datasource_id,
+                        indexhtml))
+        db.commit()
+        print(proj_unixname, " inserted into indexes table!\n")
+    except pymysql.Error as err:
+        print(err)
+        db.rollback()
+
+
+def runLicenses():
+    try:
+        cursor.execute(insertLicensesQuery,
+                       (proj_num,
+                        licenses,
+                        datasource_id))
+        db.commit()
+        print(proj_unixname, " inserted into licenses table!\n")
+    except pymysql.Error as err:
+        print(err)
+        db.rollback()
+
 
 # establish database connection: SYR
 try:
@@ -69,12 +100,26 @@ try:
 except pymysql.Error as err:
     print(err)
 
-insertQuery = 'INSERT INTO lpd_projects (datasource_id, \
-                                         name, \
-                                         display_name, \
-                                         web_link, \
-                                         last_updated) \
-            VALUES(%s, %s, %s, %s, now())'
+insertProjectsQuery = 'INSERT INTO fsf_projects (datasource_id, \
+                                                 proj_num, \
+                                                 proj_unixname, \
+                                                 url, \
+                                                 real_url, \
+                                                 desc_short, \
+                                                 date_collected) \
+                        VALUES(%s, %s, %s, %s, %s, %s, now())'
+
+insertIndexesQuery = 'INSERT INTO fsf_project_indexes (proj_num, \
+                                                       datasource_id, \
+                                                       indexhtml, \
+                                                       date_collected) \
+                        VALUES (%s, %s, %s, now())'
+
+insertLicensesQuery = 'INSERT into fsf_project_licenses (proj_num, \
+                                                         license, \
+                                                         datasource_id, \
+                                                         date_collected) \
+                        VALUES (%s, %s, %s, now())'
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -83,45 +128,74 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 
-numResults = 'of\s*(.*?)\s*results'
-results = re.findall(numResults, str(soup))[0]
+try:
+    urlResults = 'https://directory.fsf.org/wiki/All'
+    req3 = urllib2.Request(urlResults, headers=hdr)
+    html3 = urllib2.urlopen(req3, context=ssl._create_unverified_context()).read()
+    soup2 = BeautifulSoup(html3, 'html.parser')
+    num = 0
+    i = 1
+    numResults = 'There are currently (.*?) approved packages'
+    results = re.findall(numResults, str(soup2))[0]
 
-num = 0
-i = 1
+    while num < int(results):
+        url = 'https://directory.fsf.org/wiki?title=Special:Ask&offset=' + str(num) + '&limit=500&q=%5B%5BName%3A%3A%2B%5D%5D&p=30px-5D-5D%2C%3DGNU-3F%2Fmainlabel%3D%2Fformat%3Dtable&po=%3FShort+description%3DDescription%0A%3FHomepage+URL%3DHomepage%0A%3FLicense%0A%3FIs+GNU%23%5B%5BFile%3AHeckert_gnu.small.png%0A'
+        req = urllib2.Request(url, headers=hdr)
+        html = urllib2.urlopen(req, context=ssl._create_unverified_context()).read()
 
-while num < int(results):
-    listOfProjectUrl = 'https://launchpad.net/projects/+all?batch=300&memo=' + str(num) + '&start=' + str(num)
+        soup = BeautifulSoup(html, 'html.parser')
 
-    req = urllib2.Request(listOfProjectUrl, headers=hdr)
-    listOfProjectHtml = urllib2.urlopen(req).read()
+        table = soup.find('table', {'class': 'sortable wikitable smwtable'})
 
-    soup = BeautifulSoup(listOfProjectHtml, 'html.parser')
+        tr = table.find_all('tr')
+        for t in tr:
+            proj_num = i
+            print('project number: ', proj_num)
 
-    table =soup.find('table', id = 'product-listing')
+            td = t.find_all('td')
 
-    classProduct = 'class=\"sprite product\" href=\"/(.*?)\">(.*?)</a>'
-    classImage = '<div>\s*<a class=\"bg-image\" href=\"/(.*?)\" style=\"background-image: url(.*?)\">(.*?)</a>'
-    for line in table:
-        product = re.findall(classProduct, str(line))
-        if product:
-            productInfo = product[0]
-            name = productInfo[0]
-            displayName = productInfo[1]
-            web_link = 'https://launchpad.net/' + name
-            print(i, 'name: ', name, '\n    display name: ', displayName)
-            print('    web link: ', web_link)
+            proj_unixname = td[0].contents[0].contents[0]
+            print('project unixname: ', proj_unixname)
+
+            regexUrl = '<a href="/wiki/(.*?)"'
+            urlFinder = re.findall(regexUrl, str(td[0]))
+            if urlFinder:
+                url2 = 'https://directory.fsf.org/wiki/' + urlFinder[0]
+                req2 = urllib2.Request(url2, headers=hdr)
+                indexhtml = urllib2.urlopen(req2, context = ssl._create_unverified_context()).read()
+
+            desc_short = td[1].contents[0]
+            print('short description: ', desc_short)
+
+            real_url = td[2].contents[0].contents[0]
+            print('real url: ', real_url)
+
+            licenseLine = td[3]
+
+            regex = 'title=\"(.*?)\"'
+            regexFinder = re.findall(regex, str(licenseLine))
+            if regexFinder:
+                for r in regexFinder:
+                    if '(page does not exist)' in r:
+                        licenses = r.split(' (')[0]
+                        print('license: ', licenses)
+                        runLicenses()
+                    else:
+                        if 'License:' in r:
+                            licenses = r.split(':')[1]
+                            print('license: ', licenses)
+                            runLicenses()
+                        else:
+                            licenses = r
+                            print('license: ', licenses)
+                            runLicenses()
+
+            runIndexes()
+            runProjects()
             i = i + 1
-            insert()
+        num = num + 500
 
-        productWithImage = re.findall(classImage, str(line))
-        if productWithImage:
-            productWithImage = productWithImage[0]
-            name = productWithImage[0]
-            displayName = productWithImage[2]
-            web_link = 'https://launchpad.net/' + name
-            print('*', i, 'name: ', name, '\n    display name: ', displayName)
-            print('    web link: ', web_link)
-            i = i + 1
-            insert()
-
-    num = num + 300
+except pymysql.Error as err:
+    print(err)
+except urllib2.HTTPError as herror:
+    print(herror)
